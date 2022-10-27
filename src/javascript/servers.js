b67f773628f1beade820eb7d4904ab3fa4a2ac86
zachweinstein <zachary.r.weinstein@gmail.com>
2022-10-26 21:44:00 -0700

    create servers menu (unfinished)

---
+++ src/javascript/servers.js
@@ -0,0 +1,12 @@
+class ServerList extends React.Component{
+    render(){
+        return(
+            <button className="circle">
+                {/*TODO*/}
+            </button>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ServerList />);
